3894f9140bdc513529c8ff6312d4154aa92f00b0
Justin Salamon <justin.salamon@gmail.com>
2017-07-18 19:19:53 -0400

    Use he initialization in conv layer, remove data format (default is same so should be fine)

--- models/02-train.py
+++ models/02-train.py
@@ -158,7 +158,8 @@ def construct_model(pump, alpha):
     conv1 = K.layers.Convolution1D(64, 3,
                                    padding='same',
                                    activation='relu',
-                                   data_format='channels_last')(x_sq)
+                                   kernel_initializer='he_unifrom')(x_sq)
+                                   # data_format='channels_last')(x_sq)
 
     # First recurrent layer: a 128-dim bidirectional gru
     rnn1 = K.layers.Bidirectional(K.layers.GRU(128,
