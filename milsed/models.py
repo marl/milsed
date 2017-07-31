214494b72a0efa19090c9e2d89d5618c7d431e90
Justin Salamon <justin.salamon@gmail.com>
2017-07-31 16:08:23 -0400

    Add crnn2d3_smp

--- milsed/models.py
+++ milsed/models.py
@@ -610,6 +610,75 @@ def construct_crnn2d2_smp(pump, alpha):
     return model, model_inputs, model_outputs
 
 
+def construct_crnn2d3_smp(pump, alpha):
+    '''
+    CRNN with 2D conv encoder and Bi-GRU, smaller than crnn2d_smp
+
+    Parameters
+    ----------
+    pump
+    alpha
+
+    Returns
+    -------
+
+    '''
+    model_inputs = ['mel/mag']
+
+    # Build the input layer
+    layers = pump.layers()
+
+    x_mag = layers['mel/mag']
+
+    # Apply batch normalization
+    x_bn = K.layers.BatchNormalization()(x_mag)
+
+    # x_sq = milsed.layers.SqueezeLayer()(x_bn)
+
+    # First convolutional filter: 3x3
+    conv1 = K.layers.Convolution2D(16, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(x_bn)
+
+    bn2 = K.layers.BatchNormalization()(conv1)
+
+    conv2 = K.layers.Convolution2D(32, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(bn2)
+
+    bn3 = K.layers.BatchNormalization()(conv2)
+
+    conv_sq = K.layers.Convolution2D(64, (1, 128),
+                                     padding='valid',
+                                     activation='relu',
+                                     kernel_initializer='he_normal')(bn3)
+
+    sq2 = milsed.layers.SqueezeLayer(axis=-2)(conv_sq)
+
+    # First recurrent layer: a 128-dim bidirectional gru
+    rnn1 = K.layers.Bidirectional(K.layers.GRU(128,
+                                               return_sequences=True))(sq2)
+
+    n_classes = pump.fields['static/tags'].shape[0]
+
+    p0 = K.layers.Dense(n_classes, activation='sigmoid')
+
+    p_dynamic = K.layers.TimeDistributed(p0, name='dynamic/tags')(rnn1)
+
+    p_static = milsed.layers.SoftMaxPool(alpha=alpha,
+                                         axis=1,
+                                         name='static/tags')(p_dynamic)
+
+    model = K.models.Model([x_mag],
+                           [p_dynamic, p_static])
+
+    model_outputs = ['dynamic/tags', 'static/tags']
+
+    return model, model_inputs, model_outputs
+
+
 MODELS = {'crnn1d_smp': construct_crnn1d_smp,
           'crnn1d_max': construct_crnn1d_max,
           'crnn1d_avg': construct_crnn1d_avg,
@@ -619,5 +688,6 @@ MODELS = {'crnn1d_smp': construct_crnn1d_smp,
           'crnn2d_smp': construct_crnn2d_smp,
           'cnn1d2_smp': construct_cnn1d2_smp,
           'cbhg_smp': construct_cbhg_smp,
-          'crnn2d2_smp': construct_crnn2d2_smp}
+          'crnn2d2_smp': construct_crnn2d2_smp,
+          'crnn2d3_smp': construct_crnn2d3_smp}
 
