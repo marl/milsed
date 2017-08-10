9e22b1f4a0207e65da56409a31df95d678c0030b
Justin Salamon <justin.salamon@gmail.com>
2017-08-10 17:19:08 -0400

    Implement crnnL3_7_auto_2 and fix typo in model dict

--- milsed/models.py
+++ milsed/models.py
@@ -1892,6 +1892,131 @@ def construct_crnnL3_7_auto(pump, alpha):
     return model, model_inputs, model_outputs
 
 
+def construct_crnnL3_7_auto_2(pump, alpha):
+    '''
+    Like crnnL3_7_auto but with log/exp before/after autopool
+
+    Parameters
+    ----------
+    pump
+    alpha
+
+    Returns
+    -------
+
+    '''
+    log_layer = Lambda(lambda x: K.backend.log(x))
+    exp_layer = Lambda(lambda x: K.backend.exp(x))
+
+    model_inputs = ['mel/mag']
+
+    # Build the input layer
+    layers = pump.layers()
+
+    x_mag = layers['mel/mag']
+
+    # Apply batch normalization
+    x_bn = K.layers.BatchNormalization()(x_mag)
+
+    # x_sq = milsed.layers.SqueezeLayer()(x_bn)
+
+    # BLOCK 1
+    conv1 = K.layers.Convolution2D(16, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(x_bn)
+    bn1 = K.layers.BatchNormalization()(conv1)
+    conv2 = K.layers.Convolution2D(16, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(bn1)
+    bn2 = K.layers.BatchNormalization()(conv2)
+    pool2 = K.layers.MaxPooling2D((2,2), padding='valid')(bn2)
+
+    # BLOCK 2
+    conv3 = K.layers.Convolution2D(32, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(pool2)
+    bn3 = K.layers.BatchNormalization()(conv3)
+    conv4 = K.layers.Convolution2D(32, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(bn3)
+    bn4 = K.layers.BatchNormalization()(conv4)
+    pool4 = K.layers.MaxPooling2D((2, 2), padding='valid')(bn4)
+
+    # BLOCK 3
+    conv5 = K.layers.Convolution2D(64, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(pool4)
+    bn5 = K.layers.BatchNormalization()(conv5)
+    conv6 = K.layers.Convolution2D(64, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(bn5)
+    bn6 = K.layers.BatchNormalization()(conv6)
+    pool6 = K.layers.MaxPooling2D((2, 2), padding='valid')(bn6)
+
+    # BLOCK 4
+    conv7 = K.layers.Convolution2D(128, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(pool6)
+    bn7 = K.layers.BatchNormalization()(conv7)
+    conv8 = K.layers.Convolution2D(128, (3, 3),
+                                   padding='same',
+                                   activation='relu',
+                                   kernel_initializer='he_normal')(bn7)
+    bn8 = K.layers.BatchNormalization()(conv8)
+    pool8 = K.layers.MaxPooling2D((2, 2), padding='valid')(bn8)
+
+    # CONV SQUEEZE
+    conv_sq = K.layers.Convolution2D(256, (1, 8),
+                                     padding='valid',
+                                     activation='relu',
+                                     kernel_initializer='he_normal')(pool8)
+    bn8 = K.layers.BatchNormalization()(conv_sq)
+    sq2 = milsed.layers.SqueezeLayer(axis=-2)(bn8)
+
+    # RNN
+    # First recurrent layer: a 128-dim bidirectional gru
+    rnn1 = K.layers.Bidirectional(K.layers.GRU(128,
+                                               return_sequences=True))(sq2)
+
+    rnn2 = K.layers.Bidirectional(K.layers.GRU(128,
+                                               return_sequences=True))(rnn1)
+
+    rnn3 = K.layers.Bidirectional(K.layers.GRU(128,
+                                               return_sequences=True))(rnn2)
+
+    n_classes = pump.fields['static/tags'].shape[0]
+
+    p0 = K.layers.Dense(n_classes, activation='sigmoid',
+                        bias_regularizer=K.regularizers.l2())
+
+    p_dynamic = K.layers.TimeDistributed(p0, name='dynamic/tags')(rnn3)
+
+    # LOG input data to autopool
+    p_log = log_layer(p_dynamic)
+
+    p_static = milsed.layers.AutoPool(
+        axis=1,
+        kernel_constraint=K.constraints.non_neg(),
+        kernel_regularizer=K.regularizers.l2(l=1e-4))(p_log)
+
+    # EXP autopool output
+    p_static_exp = exp_layer(p_static, name='static/tags')
+
+    model = K.models.Model([x_mag],
+                           [p_dynamic, p_static_exp])
+
+    model_outputs = ['dynamic/tags', 'static/tags']
+
+    return model, model_inputs, model_outputs
+
+
 def construct_crnnL3_8_smp(pump, alpha):
     '''
     Like crnnL3_6 but has 3 BiGRU layers.
@@ -2681,5 +2806,6 @@ MODELS = {'crnn1d_smp': construct_crnn1d_smp,
           'crnnL3_10_smp': construct_crnnL3_10_smp,
           'crnnL3_11_smp': construct_crnnL3_11_smp,
           'crnnL3_12_smp': construct_crnnL3_12_smp,
-          'crnnL3_7_autio': construct_crnnL3_7_auto}
+          'crnnL3_7_auto': construct_crnnL3_7_auto,
+          'crnnL3_7_auto_2': construct_crnnL3_7_auto_2}
 
