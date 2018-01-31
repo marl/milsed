75775c4cd2dcfe1cdd4976838963927f61c677cf
Brian McFee <brian.mcfee@nyu.edu>
2018-01-31 10:22:26 -0500

    reverted augmentation index search

--- models_dcase2/02_train.py
+++ models_dcase2/02_train.py
@@ -160,8 +160,9 @@ def data_generator(working, tracks, sampler, k, augment=True, augment_drc=True,
         seeds.append(pescador.Streamer(data_sampler, fname, sampler))
 
         if augment:
-            for augname in sorted(glob(os.path.join(working,
-                                                    '{}.*.h5'.format(track)))):
+            for aug in range(10):
+                augname = fname.replace('.h5', '.{:d}.h5'.format(aug))
+                # seeds.append(pescador.Streamer(data_sampler, fname, sampler))
                 seeds.append(pescador.Streamer(data_sampler, augname, sampler))
 
         if augment_drc:
